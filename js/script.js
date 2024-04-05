1d3fb3495b7815efecf378b05195d6ee5356b404
nino <ninosopoian@gmail.com>
2024-04-05 18:00:40 +0400

    fix bags

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const Header = document.getElementById("header");
 const nav = document.getElementById("nav");
 const signIn = document.getElementById("sign-in");
 const burger = document.getElementById("burger-menu");
+const navList = document.getElementById("nav-list");
+
+const mobileNav = document.querySelector(".mobile-menu__navigation");
 let scrollTrigger = 60;
 
 // active header on scroll
@@ -9,24 +12,25 @@ window.onscroll = function () {
   // We add pageYOffset for compatibility with IE.
   if (window.scrollY >= scrollTrigger || window.scrollY >= scrollTrigger) {
     header.classList.add("active-header");
-   
+    // nav.classList.add("active-nav");
   } else {
     header.classList.remove("active-header");
+    // nav.classList.remove("active-nav");
   }
 };
 
 // sign in form popup
 
 function openForm() {
-   header.classList.toggle("active-header");
+  // navList.classList.toggle("active-nav");
+  header.classList.toggle("active-header");
   signIn.classList.toggle("hidden");
   signIn.classList.toggle("signin");
-  
 }
-
 
 // burger
 function openMobileMenu() {
-  burger.classList.toggle("open")
-  
+  header.classList.toggle("active-header");
+  burger.classList.toggle("open");
+  mobileNav.classList.add("show-navigation");
 }
