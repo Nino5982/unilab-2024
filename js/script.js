643ada0ded057dabecb7db0d3efe4f484523866f
nino <ninosopoian@gmail.com>
2024-04-08 23:39:47 +0400

    add carousel js

--- js/script.js
+++ js/script.js
@@ -55,6 +55,28 @@ addEventListener("resize", (event) => {
 
 $(document).ready(function(){
   $('.slider').slick({
-    
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
   });
 });
